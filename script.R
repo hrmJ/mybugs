dc3de2d9c8b716a2dc5d7f137696346e8f76f40a
Juho Härme <juho.harme@uta.fi>
2017-05-05 21:43:26 +0300

    One version of trying to set multi-level predictor

--- script.R
+++ script.R
@@ -11,21 +11,22 @@ if(!exists("aineisto")){
 
 #Muita mahdollisia muuttujia: funct, clausestatus 
 
-observations=xtabs(~ lang + morph + location, data=aineisto)
-totals=xtabs(~ lang + morph, data=aineisto)
+observations=xtabs(~ lang + morph + ref + location, data=aineisto)
+totals=xtabs(~ lang + morph + ref, data=aineisto)
 
 
 dataList  <-  list(observations=observations,
                    totals=totals,
                    nLang=length(unique(aineisto$lang)),
                    nMorph=length(unique(aineisto$morph)),
+                   nRef=length(unique(aineisto$ref)),
                    Nlocations = 4)
 
-monitor <- c("morph.lang")
+monitor <- c("b.ref")
 
-jagsModel <- jags.model("model.bugs", data=dataList, n.chains = 1, n.adapt = 500)
-update(jagsModel, n.iter=500)
-post <- coda.samples(jagsModel, variable.names=monitor, n.iter=1000, thin=1) 
+jagsModel <- jags.model("model.bugs", data=dataList, n.chains = 1, n.adapt = 4000)
+update(jagsModel, n.iter=4000)
+post <- coda.samples(jagsModel, variable.names=monitor, n.iter=4000, thin=1) 
 show(summary(post)$statistics)
 #mcmcplot(post)
 #pannaan talteen probsit, niin saadaan 3-ulotteinen taulukko todennäköisyydet
