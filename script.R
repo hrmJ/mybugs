43b1ad26ac77b8c8d3de54f1c90d424e7295af2f
Juho Härme <juho.harme@uta.fi>
2017-05-08 16:41:54 +0300

    before implementing more predictors

--- script.R
+++ script.R
@@ -3,6 +3,7 @@
 library(rjags)
 library(coda)
 library(mcmcplots)
+library(ggmcmc)
 
 if(!exists("aineisto")){
     aineisto <- readRDS("../koko_aineisto.rds")
@@ -22,11 +23,29 @@ dataList  <-  list(observations=observations,
                    nRef=length(unique(aineisto$ref)),
                    Nlocations = 4)
 
-monitor <- c("b.lang.ref")
+monitor <- c("lang.morph")
 
-jagsModel <- jags.model("model.bugs", data=dataList, n.chains = 2, n.adapt = 10000)
-update(jagsModel, n.iter=15000)
-post <- coda.samples(jagsModel, variable.names=monitor, n.iter=15000, thin=1) 
-show(summary(post)$statistics)
-mcmcplot(post)
+
+jagsModel <- jags.model("model.bugs", data=dataList, n.chains = 2, n.adapt = 15000)
+update(jagsModel, n.iter=20000)
+post <- coda.samples(jagsModel, variable.names=monitor, n.iter=20000, thin=1) 
+#show(summary(post)$statistics)
+#mcmcplot(post)
 #pannaan talteen probsit, niin saadaan 3-ulotteinen taulukko todennäköisyydet
+
+
+
+#intercepts <- GetLabelNamesForGgMcmc("b.lang.ref",list(levels(aineisto$lang),levels(aineisto$ref), levels(aineisto$loc)))
+intercepts <- GetLabelNamesForGgMcmc("lang.morph",list(levels(aineisto$lang),levels(aineisto$morph), levels(aineisto$loc)))
+intercepts$lang <- "fi"
+intercepts$lang[grepl("ru,",intercepts$Label)] <- "ru"
+
+intercepts$loc <- "S1"
+intercepts$loc[grepl("S2",intercepts$Label)] <- "S2"
+intercepts$loc[grepl("S3",intercepts$Label)] <- "S3"
+intercepts$loc[grepl("S4",intercepts$Label)] <- "S4"
+
+#
+ggs_caterpillar(ggs(post, par_labels=intercepts),family="ADV") + aes(color=lang) + facet_wrap(~ loc)
+
+
