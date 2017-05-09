b6949f9c5b0af7f9e2009b05e5fe3ff7bf1386b0
Juho Härme <juho.harme@uta.fi>
2017-05-09 11:36:30 +0300

    funct liian raskas

--- script.R
+++ script.R
@@ -4,6 +4,7 @@ library(rjags)
 library(coda)
 library(mcmcplots)
 library(ggmcmc)
+source("functions.R")
 
 if(!exists("aineisto")){
     aineisto <- readRDS("../koko_aineisto.rds")
@@ -12,8 +13,8 @@ if(!exists("aineisto")){
 
 #Muita mahdollisia muuttujia: funct, clausestatus 
 
-observations=xtabs(~ lang + morph + ref + location, data=aineisto)
-totals=xtabs(~ lang + morph + ref, data=aineisto)
+observations=xtabs(~ lang + morph + ref + funct + location, data=aineisto)
+totals=xtabs(~ lang + morph + ref + funct, data=aineisto)
 
 
 dataList  <-  list(observations=observations,
@@ -21,31 +22,33 @@ dataList  <-  list(observations=observations,
                    nLang=length(unique(aineisto$lang)),
                    nMorph=length(unique(aineisto$morph)),
                    nRef=length(unique(aineisto$ref)),
+                   nFunct=length(unique(aineisto$funct)),
                    Nlocations = 4)
 
-monitor <- c("lang.morph")
+monitor <- c("funct")
 
 
-jagsModel <- jags.model("model.bugs", data=dataList, n.chains = 2, n.adapt = 15000)
-update(jagsModel, n.iter=20000)
-post <- coda.samples(jagsModel, variable.names=monitor, n.iter=20000, thin=1) 
-#show(summary(post)$statistics)
+jagsModel <- jags.model("model.bugs", data=dataList, n.chains = 1, n.adapt = 500)
+update(jagsModel, n.iter=500)
+post <- coda.samples(jagsModel, variable.names=monitor, n.iter=1000, thin=1) 
+show(summary(post)$statistics)
 #mcmcplot(post)
 #pannaan talteen probsit, niin saadaan 3-ulotteinen taulukko todennäköisyydet
 
 
 
 #intercepts <- GetLabelNamesForGgMcmc("b.lang.ref",list(levels(aineisto$lang),levels(aineisto$ref), levels(aineisto$loc)))
-intercepts <- GetLabelNamesForGgMcmc("lang.morph",list(levels(aineisto$lang),levels(aineisto$morph), levels(aineisto$loc)))
-intercepts$lang <- "fi"
-intercepts$lang[grepl("ru,",intercepts$Label)] <- "ru"
-
-intercepts$loc <- "S1"
-intercepts$loc[grepl("S2",intercepts$Label)] <- "S2"
-intercepts$loc[grepl("S3",intercepts$Label)] <- "S3"
-intercepts$loc[grepl("S4",intercepts$Label)] <- "S4"
+#intercepts <- GetLabelNamesForGgMcmc("lang.morph",list(levels(aineisto$lang),levels(aineisto$morph), levels(aineisto$loc)))
+#intercepts <- GetLabelNamesForGgMcmc("funct",list(levels(aineisto$funct), levels(aineisto$loc)))
+#intercepts$lang <- "fi"
+#intercepts$lang[grepl("ru,",intercepts$Label)] <- "ru"
+#intercepts$loc <- "S1"
+#intercepts$loc[grepl("S2",intercepts$Label)] <- "S2"
+#intercepts$loc[grepl("S3",intercepts$Label)] <- "S3"
+#intercepts$loc[grepl("S4",intercepts$Label)] <- "S4"
 
 #
-ggs_caterpillar(ggs(post, par_labels=intercepts),family="ADV") + aes(color=lang) + facet_wrap(~ loc)
+#ggs_caterpillar(ggs(post, par_labels=intercepts),family="ADV") + aes(color=lang) + facet_wrap(~ loc)
+#ggs_caterpillar(ggs(post, par_labels=intercepts)) 
 
 
