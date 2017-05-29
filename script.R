e918002b2fe618fcb762ada68bf66b6985cbc0d2
Juho Härme <juho.harme@uta.fi>
2017-05-29 15:03:59 +0300

    first version with only 3 locations

--- script.R
+++ script.R
@@ -5,17 +5,18 @@ library(runjags)
 library(coda)
 library(mcmcplots)
 library(ggmcmc)
-source("functions.R")
 
 if(!exists("aineisto")){
-    aineisto <- readRDS("/home/juho_harme/phdmanuscript/monograph/data/dumps/vaihe1df.rds")
+    aineisto <- readRDS("../vaihe1df.rds")
 }
+source("../functions.R")
 
+aineisto$location[aineisto$location %in% c("S2","S3")] <- "S2/S3"
 
 #Muita mahdollisia muuttujia: funct, clausestatus 
 
-observations=xtabs(~ lang + morph + ref + funct + clausestatus + subjtype + objtype + location, data=aineisto)
-totals=xtabs(~ lang + morph + ref + funct + clausestatus + subjtype + objtype, data=aineisto)
+observations=xtabs(~ lang + morph + ref + funct + clausestatus + location, data=aineisto)
+totals=xtabs(~ lang + morph + ref + funct + clausestatus, data=aineisto)
 
 
 dataList  <-  list(observations=observations,
@@ -25,60 +26,29 @@ dataList  <-  list(observations=observations,
                    nRef=length(unique(aineisto$ref)),
                    nFunct=length(unique(aineisto$funct)),
                    nClausestatus=length(unique(aineisto$clausestatus)),
-                   Nlocations = 4)
+                   Nlocations = length(unique(aineisto$location)))
 
-monitor <- c("b.funct","b.ref","morph","lang.morph","b.lang.ref","b.lang.funct","b.clausestatus","subjtype","objtype",
-             "std.lang"," std.morph"," std.funct"," std.ref"," std.lang.morph"," std.lang.ref"," std.lang.funct",
-             "std.lang.clausestatus","std.lang.objtype","std.lang.subjtype")
+monitor <- c("b.funct","b.ref","morph","lang.morph","b.lang.ref","b.lang.funct","b.clausestatus", "std.clausestatus",
+             "std.lang","std.morph"," std.funct"," std.ref","std.lang.morph","std.lang.ref","std.lang.funct",
+             "std.lang.clausestatus","b.lang.clausestatus")
 
 
-con <- file(paste("~/public_html/currentlog.txt"))
+
+con <- file("~/public_html/currentlog.txt")
 sink(con, append=TRUE)
-
 x <- Sys.time()
 show(x)
 
-result <- autorun.jags("model.bugs", monitor = monitor, data = dataList, 
-                     summarise = TRUE, interactive = FALSE, 
-                     method = "parallel",adapt=60000,
-                     jags.refresh = 60) #refresh: kuinka usein (sekunneissa) katsotaan, onko edistystä tullut
+RunJagsModel <- run.jags(data=dataList, monitor=monitor, model="model.bugs",
+                            n.chains=2, adapt=40000, burnin=5000, thin=1,
+                            method="parallel", sample=20000)
 
-show(Sys.time()-x)
+saveRDS(RunJagsModel,"THREELOC_runjags_20000_withclausestatus.rds")
+saveRDS(RunJagsModel,"~/phdmanuscript/monograph/data/dumps/THREELOC_runjags_20000_withclausestatus.rds")
+print("SAVED.")
+
 
 sink() 
-sink(type="message")
-
-
-#time.adapt <- system.time(jagsModel <- jags.model("model.bugs", data=dataList, n.chains = 1, n.adapt = 200000))
-#show(time.adapt)
-
-#time.update <- system.time(update(jagsModel, n.iter=2000))
-#show(time.update)
-#time.sample <- system.time(post <- coda.samples(jagsModel, variable.names=monitor, n.iter=20000, thin=1))
-#
-#sum.saved <- summary(post)$statistics
-#show(sum.saved)
-#show(time.update)
-#mcmcplot(post)
-#pannaan talteen probsit, niin saadaan 3-ulotteinen taulukko todennäköisyydet
-
-
-
-#intercepts <- GetLabelNamesForGgMcmc("b.lang.ref",list(levels(aineisto$lang),levels(aineisto$ref), levels(aineisto$loc)))
-#intercepts <- GetLabelNamesForGgMcmc("lang.morph",list(levels(aineisto$lang),levels(aineisto$morph), levels(aineisto$loc)))
-#intercepts <- GetLabelNamesForGgMcmc("funct",list(levels(aineisto$funct), levels(aineisto$loc)))
-#intercepts$lang <- "fi"
-#intercepts$lang[grepl("ru,",intercepts$Label)] <- "ru"
-#intercepts$loc <- "S1"
-#intercepts$loc[grepl("S2",intercepts$Label)] <- "S2"
-#intercepts$loc[grepl("S3",intercepts$Label)] <- "S3"
-#intercepts$loc[grepl("S4",intercepts$Label)] <- "S4"
-
-#
-#ggs_caterpillar(ggs(post, par_labels=intercepts),family="ADV") + aes(color=lang) + facet_wrap(~ loc)
-#ggs_caterpillar(ggs(post, par_labels=intercepts)) 
-
-#saveRDS(post,"../phdmanuscript/monograph/data/dumps/hierarchical_dirichlecht_morph-ref-funct-clausestatus-subjtype-objtype.rds")
-
 
 close(con)
+
