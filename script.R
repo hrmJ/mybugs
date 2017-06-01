d68788b8a6f77675bdf6a2535b07ea01f1f27641
Juho Härme <juho.harme@uta.fi>
2017-06-01 16:15:47 +0300

    Uusin malli, jossa mukana pos

--- script.R
+++ script.R
@@ -7,29 +7,31 @@ library(mcmcplots)
 library(ggmcmc)
 
 if(!exists("aineisto")){
-    aineisto <- readRDS("../vaihe1df.rds")
+    #aineisto <- readRDS("../vaihe1df.rds")
+    aineisto <- readRDS("~/phdmanuscript/monograph/data/dumps/vaihe1df.rds")
 }
-source("../functions.R")
+source("functions.R")
 
+aineisto$location <- as.character(aineisto$location)
 aineisto$location[aineisto$location %in% c("S2","S3")] <- "S2/S3"
+aineisto$location <- as.factor(aineisto$location)
 
 #Muita mahdollisia muuttujia: funct, clausestatus 
 
-observations=xtabs(~ lang + morph + ref + funct + clausestatus + location, data=aineisto)
-totals=xtabs(~ lang + morph + ref + funct + clausestatus, data=aineisto)
+observations=xtabs(~ lang + pos +  funct + clausestatus + location, data=aineisto)
+totals=xtabs(~ lang + pos +  funct + clausestatus, data=aineisto)
 
 
 dataList  <-  list(observations=observations,
                    totals=totals,
                    nLang=length(unique(aineisto$lang)),
-                   nMorph=length(unique(aineisto$morph)),
-                   nRef=length(unique(aineisto$ref)),
+                   nMorph=length(unique(aineisto$pos)),
                    nFunct=length(unique(aineisto$funct)),
                    nClausestatus=length(unique(aineisto$clausestatus)),
                    Nlocations = length(unique(aineisto$location)))
 
-monitor <- c("b.funct","b.ref","morph","lang.morph","b.lang.ref","b.lang.funct","b.clausestatus", "std.clausestatus",
-             "std.lang","std.morph"," std.funct"," std.ref","std.lang.morph","std.lang.ref","std.lang.funct",
+monitor <- c("b.funct","pos","lang.pos","b.lang.funct","b.clausestatus", "std.clausestatus",
+             "std.lang","std.pos","std.funct","std.lang.pos","std.lang.funct",
              "std.lang.clausestatus","b.lang.clausestatus")
 
 
@@ -40,15 +42,21 @@ x <- Sys.time()
 show(x)
 
 RunJagsModel <- run.jags(data=dataList, monitor=monitor, model="model.bugs",
-                            n.chains=2, adapt=40000, burnin=5000, thin=1,
-                            method="parallel", sample=20000)
+                            n.chains=2, adapt=40000, burnin=10000, thin=1,
+                            method="parallel", sample=30000)
 
-saveRDS(RunJagsModel,"THREELOC_runjags_20000_withclausestatus.rds")
-saveRDS(RunJagsModel,"~/phdmanuscript/monograph/data/dumps/THREELOC_runjags_20000_withclausestatus.rds")
+saveRDS(RunJagsModel,"simplified3LOC_runjags_20000.rds")
+saveRDS(RunJagsModel,"~/phdmanuscript/monograph/data/dumps/simplified3LOC_runjags_20000.rds")
 print("SAVED.")
 
 
 sink() 
 
 close(con)
+
+
+post <- as.mcmc.list(RunJagsModel)
+extend.jags(RunJagsModel,sample=4000)
+mcmcplot(post,dir="~/public_html/jags")
+
 
