c2df4f12b633c32d9c98878fc3185a221d61141a
hrmJ <juho.harme@uta.fi>
2017-06-09 09:42:34 +0300

    Add corpustype

--- script.R
+++ script.R
@@ -6,8 +6,7 @@ library(coda)
 library(mcmcplots)
 
 if(!exists("aineisto")){
-    #aineisto <- readRDS("../vaihe1df.rds")
-    aineisto <- readRDS("~/phdmanuscript/monograph/data/dumps/vaihe1df.rds")
+    aineisto <- readRDS("vaihe1df.rds")
 }
 source("functions.R")
 
@@ -17,8 +16,8 @@ aineisto$location <- as.factor(aineisto$location)
 
 #Muita mahdollisia muuttujia: funct, clausestatus 
 
-observations=xtabs(~ lang + pos + morph +  funct + clausestatus + location, data=aineisto)
-totals=xtabs(~ lang + pos + morph +  funct + clausestatus, data=aineisto)
+observations=xtabs(~ lang + pos + corpustype + morph +  funct + clausestatus + location, data=aineisto)
+totals=xtabs(~ lang + pos + corpustype + morph +  funct + clausestatus, data=aineisto)
 
 
 dataList  <-  list(observations=observations,
@@ -26,34 +25,35 @@ dataList  <-  list(observations=observations,
                    nLang=length(unique(aineisto$lang)),
                    nMorph=length(unique(aineisto$morph)),
                    nPos=length(unique(aineisto$pos)),
+                   nCorpus=length(unique(aineisto$corpustype)),
                    nFunct=length(unique(aineisto$funct)),
                    nClausestatus=length(unique(aineisto$clausestatus)),
                    Nlocations = length(unique(aineisto$location)))
 
-monitor <- c("lang",     "b.morph",        "pos",           "b.funct",         "b.clausestatus" ,
-             "std.lang", "std.morph",      "std.pos",       "std.funct",       "std.clausestatus" ,
-                         "b.lang.morph",   "lang.pos",      "b.lang.funct",    "b.lang.clausestatus" ,
-                         "std.lang.morph", "std.lang.pos" , "std.lang.funct" , "std.lang.clausestatus")
+monitor <- c("lang",     "b.morph",        "pos",           "b.funct",         "b.clausestatus" , "corpus",
+             "std.lang", "std.morph",      "std.pos",       "std.funct",       "std.clausestatus" , "std.corpus",
+                         "b.lang.morph",   "lang.pos",      "b.lang.funct",    "b.lang.clausestatus" , "lang.corpus",
+                         "std.lang.morph", "std.lang.pos" , "std.lang.funct" , "std.lang.clausestatus", "std.lang.corpus")
 
 
 
-con <- file("~/public_html/currentlog.txt")
+con <- file(paste("c:Users/jh89963.STAFF/Dropbox/currentlog.txt"))
 sink(con, append=TRUE)
 show(Sys.time())
 
 RunJagsModel <- run.jags(data=dataList, monitor=monitor, model="model.bugs",
-                            n.chains=2, adapt=40000, burnin=10000, thin=10,
-                            method="parallel", sample=100000)
+                            n.chains=2, adapt=100000, burnin=50000, thin=9,
+                            method="parallel", sample=80000)
 
+post <- as.mcmc.list(RunJagsModel)
+mcmcplot(post)
 saveRDS(RunJagsModel,"withpos_100k.rds")
-saveRDS(RunJagsModel,"~/phdmanuscript/monograph/data/dumps/withpos_100k.rds")
+saveRDS(RunJagsModel,"c:Users/jh89963.STAFF/Dropbox/withcorpus_800k.rds")
 print("SAVED.")
 
 sink() 
 close(con)
 
 
-post <- as.mcmc.list(RunJagsModel)
-mcmcplot(post,dir="~/public_html/jags")
 
 
